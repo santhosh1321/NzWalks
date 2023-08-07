03f118cefcb60df6bcebc97a6811a85f0b79189d
santhoshpatel1321@gmail.com <santhoshnani1321@gmail.com>
2023-08-07 16:31:11 +0530

    END OD SECTION

---
+++ NzWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Double Area { get; set; }
+        public Double Lat { get; set; }
+        public Double Long { get; set; }
+        public long Population { get; set; }
+
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
---
+++ NzWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,18 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid  Id { get; set; }
+        public string Name { get; set; }
+        public Double Length { get; set; }
+
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+
+        //Navigation Proparties
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
